d9bf537aaff947211977606207605722ee7cf122
yong <392115980@qq.com>
2018-03-22 17:05:35 +0800

    无修改

---
+++ src/com/accp/io/Test.java
@@ -0,0 +1,26 @@
+package com.accp.io;
+
+/**
+ * @Description:
+ * @Author: liyong
+ * @CreateDate: 2018/3/14 9:27
+ * @Version: 1.0
+ */
+public class Test {
+
+
+    public static void main(String[] args){
+        say("A");
+        say("A");
+        say("A");
+        say("A");
+        say("A");
+        say("A");
+        System.out.println(1);
+    }
+    public static void say(String s){
+        System.out.println("hello:"+s);
+
+    }
+
+}
